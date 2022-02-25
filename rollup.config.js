acb96af11603314d7d8633b4f9f703dd1f619f09
zck <xlszzck@qq.com>
2022-02-25 11:19:45 +0800

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -6,9 +6,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 // 一段自定义的内容，以下内容会添加到打包结果中
-const footer = `if(typeof window !== 'undefined') {
-  window._Dry_VERSION_ = '${pkg.version}'
-}`
+// const footer = `if(typeof window !== 'undefined') {
+//   window._Dry_VERSION_ = '${pkg.version}'
+// }`
 
 export default {
   input: "./src/index.ts",
