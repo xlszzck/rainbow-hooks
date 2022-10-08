f730c99716d1e9f9cab767ba926dc3fd7a4437e4
zck <xlszzck@qq.com>
2022-10-08 19:54:10 +0800

    1

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,24 +1,24 @@
 // import hash from 'hash.js';
-import { stringify } from 'qs';
+import { stringify } from "qs";
 // import { getCookie } from '../utils';
-import { InterceptorsOptions, otherOptions } from '../../typings';
+import { InterceptorsOptions, otherOptions } from "../../typings";
 
 const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。',
+  200: "服务器成功返回请求的数据。",
+  201: "新建或修改数据成功。",
+  202: "一个请求已经进入后台排队（异步任务）。",
+  204: "删除数据成功。",
+  400: "发出的请求有错误，服务器没有进行新建或修改数据的操作。",
+  401: "用户没有权限（令牌、用户名、密码错误）。",
+  403: "用户得到授权，但是访问是被禁止的。",
+  404: "发出的请求针对的是不存在的记录，服务器没有进行操作。",
+  406: "请求的格式不可得。",
+  410: "请求的资源被永久删除，且不会再得到的。",
+  422: "当创建一个对象时，发生一个验证错误。",
+  500: "服务器发生错误，请检查服务器。",
+  502: "网关错误。",
+  503: "服务不可用，服务器暂时过载或维护。",
+  504: "网关超时。",
 };
 
 // const cachedSave = (response, hashcode) => {
@@ -46,10 +46,10 @@ const codeMessage = {
  * Requests a URL, returning a promise.
  *
  * @param  {string} url       The URL we want to request
- * @param  {object} [option] The options we want to pass to "fetch"
+ * @param  {object} [option]  The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export default function request(url: string, option: InterceptorsOptions, { requestInterceptors, responseInterceptors }: otherOptions) {
+export default function request<T>(url: string, option: InterceptorsOptions, { requestInterceptors, responseInterceptors }: otherOptions): Promise<T> {
   const _requestInterceptors = requestInterceptors(url, option);
 
   url = _requestInterceptors?.url || url;
@@ -64,7 +64,7 @@ export default function request(url: string, option: InterceptorsOptions, { requ
    * Produce fingerprints based on url and parameters
    * Maybe url has the same parameters
    */
-  const fingerprint = url + (options.body ? JSON.stringify(options.body) : '');
+  const fingerprint = url + (options.body ? JSON.stringify(options.body) : "");
 
   // const hashcode = hash.sha256().update(fingerprint).digest('hex');
 
@@ -78,14 +78,14 @@ export default function request(url: string, option: InterceptorsOptions, { requ
     // headers: { Authorization: getCookie('token') },
   };
 
-  if (['POST', 'PUT', 'DELETE'].includes(newOptions.method)) {
+  if (["POST", "PUT", "DELETE"].includes(newOptions.method)) {
     const { body } = newOptions;
 
     // newOptions.body is JSON
     if (!(body instanceof FormData)) {
       newOptions.headers = {
         // Accept: 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
+        "Content-Type": "application/json; charset=utf-8",
         ...newOptions.headers,
       };
       newOptions.body = JSON.stringify(newOptions.body);
@@ -93,7 +93,7 @@ export default function request(url: string, option: InterceptorsOptions, { requ
     // newOptions.body is FormData
     else {
       newOptions.headers = {
-        Accept: 'application/json',
+        Accept: "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
         ...newOptions.headers,
       };
@@ -110,7 +110,7 @@ export default function request(url: string, option: InterceptorsOptions, { requ
   }
 
   // 拼接GET请求参数
-  if (newOptions.method === 'GET' && Object.keys(newOptions?.params || {}).length > 0) {
+  if (newOptions.method === "GET" && Object.keys(newOptions?.params || {}).length > 0) {
     url = `${url}?${stringify(newOptions.params)}`;
   }
 
